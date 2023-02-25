bc0645fa736e03b4de4d00a61ad0724e1e3a3ad0
Sampath kumar <51330761+sampathkumar26@users.noreply.github.com>
2023-02-25 09:51:35 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Analytics from "./Components/Analytics";
+import Cards from "./Components/Cards";
+import Footer from "./Components/Footer";
+import Hero from "./Components/Hero";
+import NavBar from "./Components/NavBar";
+import Newsletter from "./Components/Newsletter";
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
+    </>
   );
 }
 
